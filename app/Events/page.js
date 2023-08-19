fe128840d9ee14f34ca207ad2c6d7c903e398191
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-08-19 20:20:28 +0530

    File Structure Establishment - The actual Work Starts!

--- app/Events/page.js
+++ app/Events/page.js
@@ -3,15 +3,17 @@
 import { useRouter,  } from 'next/navigation'
 import { Button } from '@mui/material'
 import CPU from '../../public/CPU.jpeg';
+import Image from 'next/Image';
 
 export default function Page() {
     const router = useRouter()
 
     return (
 
+        
         <div>
             <div className={"background"}>
-                <img src={CPU} alt={"cpu-background"} className={"CPU"}/>
+                <Image src={CPU} alt={"cpu-background"} className={"CPU"}/>
             </div>
             <h1>Events Page</h1>
             <Button variant="contained" onClick={() => router.push('/')}>Back to HomePage</Button>
--- app/page.js
+++ app/page.js
@@ -1,62 +1,7 @@
-import Link from 'next/link';
-import CPU from '../public/CPU.jpeg';
-import { IconButton } from '@mui/material';
-import {Person, AccessAlarmRounded} from "@mui/icons-material";
-import { Card, CardContent, Typography, Grid } from '@mui/material';
-
 export default function Page() {
   return (
       <div>
-        <div className={"home-button"}>
-          <Link href="/team/">
-            <IconButton aria-label="team" size="large">
-              <Person />
-            </IconButton>
-          </Link>
-          <Link href="/Events/">
-            <IconButton aria-label="events" size="large">
-              <AccessAlarmRounded />
-            </IconButton>
-          </Link>
-        </div>
-
-        <div className={"background"}>
-          <img src={CPU} alt={"cpu-background"} className={"CPU"}/>
-        </div>
-
-        <div className={"event-cards"}>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <Grid item xs={6} className={"card"}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5">Event 1</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={6} className={"card"}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5">Event 2</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={6} className={"card"}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5">Event 3</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={6} className={"card"}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5">Event 4</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
-        </div>
-
+        {/*<h1>This Is the Home Page</h1>*/}
       </div>
   )
 }
